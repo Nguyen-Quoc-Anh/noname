1e1f07b80145c73050ea5eb0bd02200743089c63
Nguyen-Quoc-Anh <quocanhseu@gmail.com>
2021-09-29 21:20:19 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+console.log("ok");
+arrows.forEach((arrow, i) => {
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 350);
+        clickCounter++;
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+            movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 350}px)`;
+        } else {
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+});
+
+
+
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.move-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.movie-list-title,.nav-button");
+ball.addEventListener("click", ()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+});
+
+
+const tempButtons = document.querySelectorAll(".nav-button,.slide-button");
+tempButtons.forEach((tempButton) => {
+    tempButton.addEventListener("click", function(){   
+        alert("Để nút cho vui thôi chứ chưa có chức năng đâu :)");
+    });
+});
+
+
+const leftIcons = document.querySelectorAll(".left-menu-icon");
+leftIcons[0].style.marginTop = "20px";
+leftIcons[0].style.paddingRight = "15px";
+leftIcons[0].style.paddingLeft = "15px";
+leftIcons[3].style.paddingRight = "18px";
+leftIcons[3].style.paddingLeft = "18px";
+leftIcons[5].style.paddingRight = "18px";
+leftIcons[5].style.paddingLeft = "18px";
